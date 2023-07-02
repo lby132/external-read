a994f8e61e5b1895e18672a4efdfc05fd3da37ff
lby132 <quddbscjswow@naver.com>
2023-07-02 17:36:38 +0900

    properties 읽기

---
+++ src/main/java/hello/ExternalReadApplication.java
@@ -0,0 +1,20 @@
+package hello;
+
+import hello.config.*;
+import hello.datasource.MyDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
+
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
+@ConfigurationPropertiesScan
+public class ExternalReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalReadApplication.class, args);
+    }
+
+}
---
+++ src/main/java/hello/datasource/MyDataSourcePropertiesV2.java
@@ -0,0 +1,41 @@
+package hello.datasource;
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ConfigurationProperties("my.datasource")
+public class MyDataSourcePropertiesV2 {
+
+    private String url;
+    private String username;
+    private String password;
+    private Etc etc;
+
+    // @DefaultValue 는 설정파일에 값이 없을때 사용하면 기본값으로 세팅됨 ex) maxConnection=0, timeout=null
+    public MyDataSourcePropertiesV2(String url, String username, String password, @DefaultValue Etc etc) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.etc = etc;
+    }
+
+    @Getter
+    public static class Etc {
+        private int maxConnection;
+        private Duration timeout;
+        private List<String> options;
+
+        public Etc(int maxConnection, Duration timeout, @DefaultValue("DEFAULT") List<String> options) {
+            this.maxConnection = maxConnection;
+            this.timeout = timeout;
+            this.options = options;
+        }
+    }
+}
